f19f7c55cf2bf8929fd51204bbec24f864c8398a
FitsDeSpits <schuurfinn@gmail.com>
2025-05-24 14:20:23 +0200

    more stuff

---
+++ Runtime/Scripts/General/AnimatorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public static class AnimatorExtensions {
+    public static async Task WaitForCompletionAsync(this Animator animator, string trigger) {
+        if (animator == null || string.IsNullOrEmpty(trigger)) {
+            Debug.LogWarning("Animator or trigger is null/empty.");
+            return;
+        }
+        animator.SetTrigger(trigger);
+        while (!IsAnimatorPlaying(animator)) {
+            await Task.Yield();
+        }
+        while (IsAnimatorPlaying(animator)) {
+            await Task.Yield();
+        }
+    }
+
+    private static bool IsAnimatorPlaying(this Animator animator) {
+        if (animator.IsInTransition(0)) return true;
+        var stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        return stateInfo.normalizedTime < 1 && stateInfo.loop == false;
+    }
+}
---
+++ Runtime/Scripts/General/CoroutineHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class CoroutineHelper {
+    public static Task CoroutineTask(MonoBehaviour gameObject, IEnumerator coroutine) {
+        var task = new TaskCompletionSource<bool>();
+        gameObject.StartCoroutine(WaitForCompletion(coroutine, task));
+        return task.Task;
+    }
+
+    private static IEnumerator WaitForCompletion(IEnumerator coroutine, TaskCompletionSource<bool> task) {
+        yield return coroutine;
+        task.SetResult(true);
+    }
+}
