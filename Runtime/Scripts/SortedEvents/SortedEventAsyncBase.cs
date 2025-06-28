7f43104fd8c935dd22870de85216fed69ba1788a
finnschuuring <schuurfinn@gmail.com>
2025-06-28 15:44:46 +0200

    1.1.10

--- Runtime/Scripts/SortedEvents/SortedEventAsyncBase.cs
+++ Runtime/Scripts/SortedEvents/SortedEventAsyncBase.cs
@@ -73,4 +73,40 @@
             await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6));
         }
     }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7));
+        }
+    }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7, T8> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, T8, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7, t8));
+        }
+    }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7, t8, t9));
+        }
+    }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10));
+        }
+    }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11));
+        }
+    }
+
+    public class SortedEventAsync<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> : SortedEventAsyncBase<Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, Task<bool>>> {
+        public async Task InvokeAsync(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12) {
+            await InvokeInternalAsync(async action => await action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12));
+        }
+    }
 }
--- Runtime/Scripts/SortedEvents/SortedEventBase.cs
+++ Runtime/Scripts/SortedEvents/SortedEventBase.cs
@@ -71,4 +71,40 @@
             InvokeInternal(action => action(t1, t2, t3, t4, t5, t6));
         }
     }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7));
+        }
+    }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7, T8> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7, T8>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7, t8));
+        }
+    }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7, t8, t9));
+        }
+    }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10));
+        }
+    }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11));
+        }
+    }
+
+    public class SortedEvent<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> : SortedEventBase<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> {
+        public void Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9, T10 t10, T11 t11, T12 t12) {
+            InvokeInternal(action => action(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12));
+        }
+    }
 }
