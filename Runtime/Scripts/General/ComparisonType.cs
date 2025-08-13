35072a6cc44ff1f93fc7ae589275582f0536d5e7
FitsDeSpits <schuurfinn@gmail.com>
2025-08-13 11:52:46 +0200

    1.1.15

---
+++ Runtime/Scripts/General/ComparisonType.cs
@@ -0,0 +1,10 @@
+﻿namespace FinnSchuuring.Utilities {
+    public enum ComparisonType {
+        Equal = 0,
+        NotEqual = 1,
+        Greater = 2,
+        GreaterEqual = 3,
+        Less = 4,
+        LessEqual = 5
+    }
+}
